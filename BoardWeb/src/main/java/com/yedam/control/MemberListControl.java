8a115b6f42863f96e54d5f73dfffe881667d261a
admin <admin@DESKTOP-UBR9SPO>
2024-09-05 17:50:12 +0900

    회원조회 페이지 작업 중

--- BoardWeb/src/main/java/com/yedam/control/MemberListControl.java
+++ BoardWeb/src/main/java/com/yedam/control/MemberListControl.java
@@ -1,17 +1,29 @@
 package com.yedam.control;
 
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedam.common.Control;
 import com.yedam.service.MemberService;
 import com.yedam.service.MemberServiceImpl;
+import com.yedam.vo.MemberVO;
 
 public class MemberListControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest request, HttpServletResponse response) {
 		MemberService svc = new MemberServiceImpl();
+		List<MemberVO> list = svc.getMembers();
+		
+		request.setAttribute("list", list);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("something.jsp");
+		rd.forward(request, response);
+		
+		// TODO : 나머지 회원 조회 페이지는 BaordListControl.java를 참고하여 진행하자
 		
 
 	}
